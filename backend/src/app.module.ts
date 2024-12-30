d312a7adf6bc4e86914d56991bbf3e73811d9ae6
Maksouni <max1205325@gmail.com>
2024-12-31 00:02:32 +0700

    backend with postgres mafaka

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { TodosModule } from './todos/todos.module';
+import { Todo } from './todos/entities/todo.entity';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'mydatabase',
+      entities: [User, Todo],
+      synchronize: true,
+    }),
+    UsersModule,
+    TodosModule,
+    PrismaService,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
