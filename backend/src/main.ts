a7ed98a2acef8f3ce9c54766474b0d15f64ac023
Maksouni <max1205325@gmail.com>
2025-01-07 15:40:25 +0700

    added exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Если нужно передавать куки или заголовки авторизации
   });
+
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
